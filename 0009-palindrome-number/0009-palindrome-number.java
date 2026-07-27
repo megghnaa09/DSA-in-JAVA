4d493db9c50416ee8014dac66fc1d17a69f9b5ae
Meghna Rana <meghna1870.becse24@chitkara.edu.in>
2026-07-27 22:14:13 +0530

    Time: 5 ms (80.8%), Space: 45.9 MB (72.66%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+        return false;
+    }
+    int revHalf = 0;
+    while (x > revHalf) {
+        revHalf = revHalf * 10 + x % 10;
+        x = x / 10;
+    }
+
+    return x == revHalf || x == revHalf / 10;
+    }
+}
