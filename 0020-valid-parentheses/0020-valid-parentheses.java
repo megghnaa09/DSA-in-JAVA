a44dd100efc33ada57bd979f5a0b6d52edc59258
Meghna Rana <meghna1870.becse24@chitkara.edu.in>
2026-08-04 09:42:28 +0530

    Time: 4 ms (36.82%), Space: 43.4 MB (26.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,9 +3,6 @@ class Solution {
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-            // if(!st.isEmpty()){
-            //     char last = st.peek();
-            // }
             if(ch=='{' || ch=='[' || ch=='('){
                 st.push(ch);
             }else if(ch == '}' || ch==']' || ch==')'){
