16c894fc08b2aa9d354005025a868515f26cdb5f
Meghna Rana <meghna1870.becse24@chitkara.edu.in>
2026-08-06 10:27:34 +0530

    Time: 5 ms (81.39%), Space: 46.4 MB (74.13%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,26 +1,20 @@
 class Solution {
     public int longestValidParentheses(String s) {
-
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
-
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
         int maxLen = 0;
-
         for (int i = 0; i < s.length(); i++) {
-
             if (s.charAt(i) == '(') {
-                stack.push(i);
+                st.push(i);
             } else {
-                stack.pop();
-
-                if (stack.isEmpty()) {
-                    stack.push(i);
+                st.pop();
+                if (st.isEmpty()) {
+                    st.push(i);
                 } else {
-                    maxLen = Math.max(maxLen, i - stack.peek());
+                    maxLen = Math.max(maxLen, i - st.peek());
                 }
             }
         }
-
         return maxLen;
     }
 }
